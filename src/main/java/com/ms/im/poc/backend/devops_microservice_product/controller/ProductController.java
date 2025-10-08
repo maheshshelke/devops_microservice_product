53dfba675d73b18be45aef2c5441c0726473192b
maheshshelke <mahesh.shelke08@gmail.com>
2025-10-09 00:59:49 +0530

    fix: change products list to mutable ArrayList in ProductController

--- src/main/java/com/ms/im/poc/backend/devops_microservice_product/controller/ProductController.java
+++ src/main/java/com/ms/im/poc/backend/devops_microservice_product/controller/ProductController.java
@@ -3,16 +3,17 @@ package com.ms.im.poc.backend.devops_microservice_product.controller;
 import com.ms.im.poc.backend.devops_microservice_product.model.Product;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    List<Product> products = List.of(
+    List<Product> products = new ArrayList<>(List.of(
             new Product(1L, "Product A", 10.0),
             new Product(2L, "Product B", 20.0)
-    );
+    ));
 
     @GetMapping
     public List<Product> getAllProducts() {
